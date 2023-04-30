1e1a3180624fbdd9763a91ed59d99d2a62e6151c
SungWoo Hong <sungwoo166@gmail.com>
2023-04-30 19:51:31 +0900

    init

---
+++ src/main/java/stomp/scoketDemo/ScoketDemoApplication.java
@@ -0,0 +1,13 @@
+package stomp.scoketDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoketDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/stomp/scoketDemo/ScoketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package stomp.scoketDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoketDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
